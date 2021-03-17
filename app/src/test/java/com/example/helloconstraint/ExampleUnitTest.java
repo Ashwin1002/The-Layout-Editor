0da3f03a0bfca2e7c9fec72345726d680f7cb2ed
ashwin <ashwin.shrestha2258@gmail.com>
2021-03-17 10:55:49 +0545

    Zero button added

--- app/src/test/java/com/example/helloconstraint/ExampleUnitTest.java
+++ app/src/test/java/com/example/helloconstraint/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.example.hellotoast;
+package com.example.helloconstraint;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
